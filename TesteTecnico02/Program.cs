13d6d7f31b1a13c9819215fada15a19e40ba6655
Fernando-Faustino <fernandofaustino627@gmail.com>
2023-04-08 20:32:08 -0300

    Adicionar arquivos de projeto.

---
+++ TesteTecnico02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace testeTecnico02 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Digite um número: ");
+            int num = int.Parse(Console.ReadLine());
+
+            int a = 0, b = 1, temp;
+
+            while (b <= num) {
+                if (b == num) {
+                    Console.WriteLine("{0} pertence à sequência de Fibonacci.", num);
+                    return;
+
+                }
+                temp = a + b;
+                a = b;
+                b = temp;
+                Console.Write(temp + " ");
+
+            }
+            Console.WriteLine();
+            Console.WriteLine("{0} não pertence à sequência de Fibonacci.", num);
+
+        }
+    }
+}
